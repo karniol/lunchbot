28cefe76ee38e38c49949d06a80e67b26e8cddb7
takoss <takoss@ttu.ee>
2018-02-20 19:02:18 +0200

    Basic backend framework files.

---
+++ backend/src/main/java/com/ttu/lunchbot/LunchbotApplication.java
@@ -0,0 +1,12 @@
+package com.ttu.lunchbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchbotApplication.class, args);
+	}
+}
