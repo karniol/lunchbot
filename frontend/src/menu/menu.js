bf0b33049016579b0d41572fbb9eeed3533202ae
Charlie Karniol <charliekarniol@gmail.com>
2018-03-05 14:04:10 +0200

    Prettified frontend

--- frontend/src/menu/menu.js
+++ frontend/src/menu/menu.js
@@ -1,19 +1,16 @@
-import {HttpClient} from 'aurelia-fetch-client';
+import {Fetcher} from '../util/fetcher';
 
 export class Menu {
   constructor() {
-    this.message = 'Menu';
     this.cafeId = 1;
     this.data = null;
     this.getMenuItems();
   }
 
   getMenuItems() {
-    let httpClient = new HttpClient();
-
-    httpClient
-      .fetch(`http://localhost:8080/cafes/${this.cafeId}/menu`, {'method': 'GET'})
+    Fetcher.getInstance()
+      .fetch(`cafes/${this.cafeId}/menu`, {'method': 'GET'})
       .then(response => response.json())
-      .then(data => this.data = data)
+      .then(data => this.data = data);
   }
 }
---
+++ frontend/src/util/fetcher.js
@@ -0,0 +1,48 @@
+// Fetcher is a singleton for Aurelia's Fetch client
+
+import {HttpClient, json} from 'aurelia-fetch-client';
+
+export var Fetcher = (function () {
+  let instance;
+  
+  function createInstance() {
+    let object = new HttpClient();
+
+    // TODO: Read base url from config.json file
+    object.configure(config => {
+      config
+        .withBaseUrl("http://localhost:8080/")
+        .withDefaults({
+          headers: {
+            'Accept': 'application/json',
+            'X-Requested-With': 'Fetch'
+          }
+        })
+        .withInterceptor({
+          request(request) {
+            return request;
+          },
+          requestError(requestError) {
+            return requestError;
+          },
+          response(response) {
+            return response;
+          },
+          responseError(responseError) {
+            return responseError;
+          }
+        });
+    });
+
+    return object;
+  }
+
+  return {
+    getInstance: function () {
+      if (!instance) {
+        instance = createInstance();
+      }
+      return instance;
+    }
+  }
+})();
