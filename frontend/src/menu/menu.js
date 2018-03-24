b724323487ee395d95f320920d08f75cb69f012b
Charlie Karniol <charliekarniol@gmail.com>
2018-03-24 17:41:21 +0200

    Proper price formatting

--- frontend/src/menu/menu.js
+++ frontend/src/menu/menu.js
@@ -10,11 +10,19 @@ export class Menu {
     this.getMenuItems(params.id);
   }
 
+  static formatPriceString(data) {
+    data["menu_items"].forEach(function(menuItem) {
+      menuItem["price_string"] = "€" + menuItem["price"].toFixed(2);
+    });
+    return data
+  }
+
   getMenuItems(foodServiceId) {
     Fetcher.getInstance()
       .fetch(`foodservices/${foodServiceId}/menus/today`, {'method': 'GET'})
       .then(response => response.json())
       .then(data => this.data = data)
+      .then(() => Menu.formatPriceString(this.data))
       .then(() => this.successfulRequest = true);
   }
 }
