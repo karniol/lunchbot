7157975ee0d5dbcd2c98373c214efebc1df5f4e1
Charlie Karniol <charliekarniol@gmail.com>
2018-04-04 11:56:45 +0300

    Sticky menu overview

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -3,6 +3,10 @@ import environment from "./environment";
 export class App {
   constructor() {}
 
+  toggleSearchMenu() {
+    document.getElementById("lunchbot-id-search-menu").classList.toggle("d-none");
+  }
+
   configureRouter(config, router) {
     config.title = 'Lunchbot';
     config.options.pushState = true;
