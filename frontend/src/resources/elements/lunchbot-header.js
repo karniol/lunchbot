1e663bd3a0f72e8a5fcd0da42deefb0cac81f2ac
Charlie Karniol <charliekarniol@gmail.com>
2018-05-15 12:56:56 +0300

    Frontend lang support

--- frontend/src/resources/elements/lunchbot-header.js
+++ frontend/src/resources/elements/lunchbot-header.js
@@ -1,11 +1,21 @@
 import {Router} from "aurelia-router";
+import {I18N} from 'aurelia-i18n';
 
 export class LunchbotHeader {
-  static inject() { return [Router]; }
+  static inject() { return [Router, I18N]; }
 
-  constructor(router) {
+  constructor(router, i18n) {
     this.router = router;
+    this.i18n = i18n;
     this.searchMenuVisible = false;
+  }
+
+  switchLanguage() {
+    if (this.i18n.getLocale() == "et") {
+      this.i18n.setLocale("en");
+    } else if (this.i18n.getLocale() == "en") {
+      this.i18n.setLocale("et");
+    }
   }
 
   toggleSearchMenu() {
@@ -18,6 +28,9 @@ export class LunchbotHeader {
       this.router.navigate(`/filter/${value}/`);
       this.searchMenuVisible = false;
     }
+  }
 
+  route(path) {
+    this.router.navigate(path);
   }
 }
